347b3f11b634481b9dd587e38d633aee88bed532
kotpsot <zygmunt.justyna@gmail.com>
2018-02-27 15:41:02 +0000

    remove comments

--- controllers/kots.js
+++ controllers/kots.js
@@ -1,21 +1,10 @@
 const Kot = require('../models').Kot;
 
-// module.exports = {
-//   create(req, res) {
-//     return Kot
-//       .create({
-//         name: "Kitty",
-//       })
-//       // .then(todo => res.status(201).send(todo))
-//       // .catch(error => res.status(400).send(error));
-//   },
-// };
-
 module.exports = {
   create(req, res) {
     return Kot
       .create({
-        name: 'Kitty'
+        name: body.form.catname
       })
       // .then(todo => res.status(201).send(todo))
       // .catch(error => res.status(400).send(error));
